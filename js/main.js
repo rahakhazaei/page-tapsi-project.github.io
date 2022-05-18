96fdbea584cc8dde2b8fc1f35d094333bba72e12
somaye khazaie <somayeh.khazaie@gmail.com>
2022-05-19 00:09:52 +0430

    update travel section and create radio button and accordion section

--- js/main.js
+++ js/main.js
@@ -19,3 +19,13 @@ tabs.forEach((tab) => {
     targeTabContent.classList.add("active");
   });
 });
+
+//accordion
+const accordionHeaders = document.querySelectorAll(".accordion__header");
+      // const accordion = document.querySelector(".accordion");
+      accordionHeaders.forEach((item) => {
+        item.addEventListener("click", (e) => {
+          console.log(e.target.parentElement);
+          e.target.parentElement.classList.toggle("accordion__expanded");
+        });
+      });
